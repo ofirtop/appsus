1679781224308a04849f832f9559248489b67e81
ofirtop <ofirtopaz@gmail.com>
2019-02-27 14:40:34 +0200

    add favicon + add email list component

--- js/apps/email/pages/email-app-page.js
+++ js/apps/email/pages/email-app-page.js
@@ -1,9 +1,13 @@
+import emailList from '../cmps/email-list-cmp.js'
 
 export default {
     template: `
         <section class="email-app">
-            email app
+            <email-list></email-list>
         </section> 
     `,
+    components:{
+        emailList
+    },
     name:'email-app'
 }
