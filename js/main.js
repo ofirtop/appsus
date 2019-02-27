ac82d8f1c4831e4ac3f811a38bae9fcfea91efae
Hagar Rachman <orrrachman@gmail.com>
2019-02-27 14:56:06 +0200

    add noteService refactor

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ import myRoutes from './routes.js';
 
 const myRouter = new VueRouter({routes: myRoutes})
 
-console.log('myRoutes', myRoutes);
-
 window.vueApp = new Vue({
     el: '#app',
     router: myRouter,
