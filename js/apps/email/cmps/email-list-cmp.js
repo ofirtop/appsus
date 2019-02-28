5b1fa5cccbc333dcdadd87738a2ed3b613fa27f7
ofirtop <ofirtopaz@gmail.com>
2019-02-28 20:57:06 +0200

    create text filter

--- js/apps/email/cmps/email-list-cmp.js
+++ js/apps/email/cmps/email-list-cmp.js
@@ -4,8 +4,10 @@ import emailService from '../services/email-service-cmp.js';
 export default {
     template: `
             <section class="email-list" >
+                <input type=text placeholder="filter by text" 
+                       v-model="filterBy.text" @input="setFilter">
                 <email-preview 
-                    v-for="currEmail in emails" 
+                    v-for="currEmail in emailToShow" 
                     :key="currEmail.id"
                     :email="currEmail"
                     @mailread="onMailRead"></email-preview>
@@ -13,22 +15,39 @@ export default {
     `,
     data() {
         return {
-            emails: null
+            emails: null,
+            filterBy: {
+                text: ''
+            }
         }
     },
     components: {
         emailPreview
     },
-    methods:{
-        onMailRead(){
+    methods: {
+        onMailRead() {
             emailService.saveEmails();
+        },
+        setFilter() {
+
+        }
+    },
+    computed: {
+        emailToShow() {
+            console.log('filter.text: ',this.filterBy.text)
+            if (this.filterBy.text === '') return this.emails;
+            else {
+                return this.emails.filter(email => {
+                    return email.subject.toLowerCase().includes(this.filterBy.text.toLowerCase());
+                })
+            }
         }
     },
     created() {
         this.emails = emailService.loadEmails();
-        console.log('email-list created emails:',this.emails);
+        console.log('email-list created emails:', this.emails);
         var counter = emailService.getUnReadEmailsCounter();
-        this.$emit('UnreadEmailsCounter',counter);        
+        this.$emit('UnreadEmailsCounter', counter);
     },
     name: 'email-list'
 }
